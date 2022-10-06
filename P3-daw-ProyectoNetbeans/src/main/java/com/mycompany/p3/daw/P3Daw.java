4d7bbb1e8762171c2feefb3df7b0e45d146f752e
danicorpa <daniel.martin385@educa.madrid.org>
2022-10-06 22:28:16 +0200

    practicas

---
+++ P3-daw-ProyectoNetbeans/src/main/java/com/mycompany/p3/daw/P3Daw.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.p3.daw;
+
+/**
+ *
+ * @author xdmc0001
+ */
+public class P3Daw {
+
+    public static void main(String[] args) {
+        
+                System.out.println("Hello World!");
+                
+             
+                
+    }
+}
